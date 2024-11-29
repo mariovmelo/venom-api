ddf9faf9b60897dde6ebd551d41e91a705166dec
Mário Melo <mariovmelo@gmail.com>
2024-11-29 18:17:14 +0000

    ajustes

--- src/index.js
+++ src/index.js
@@ -58,7 +58,13 @@ class VenomManager {
       }, {
         session: sessionName,
         folderNameToken: sessionPath,
-        browserArgs:['--user-agent=Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/94.0.4606.61 Safari/537.36']
+        headless:true,
+        browserArgs:['--user-agent=Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/94.0.4606.61 Safari/537.36',
+          '--no-sandbox',
+          '--disable-setuid-sandbox',
+          '--disable-dev-shm-usage'
+        ],
+        
       })
       .then((client) => {
         this.sessions.get(sessionId).client = client;
