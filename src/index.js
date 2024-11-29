f44e46b628ea4006f18b91fc04c8c5c11714bef0
Mário Melo <mariovmelo@gmail.com>
2024-11-29 18:43:09 +0000

    ajustes

--- src/index.js
+++ src/index.js
@@ -62,7 +62,8 @@ class VenomManager {
         browserArgs:['--user-agent=Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/94.0.4606.61 Safari/537.36',
           '--no-sandbox',
           '--disable-setuid-sandbox',
-          '--disable-dev-shm-usage'
+          '--disable-dev-shm-usage',
+          '--disable-gpu'
         ],
         
       })
